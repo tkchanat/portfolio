132cc411db4030ae55ab9d2e4368bd07247c0f55
tkchanat <andychancse@gmail.com>
2023-03-15 19:20:03 -0700

    Deployed 58b7e0e with MkDocs version: 1.4.2

---
+++ wasm/flux-rs/flux_rs_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function greet(a: number, b: number): void;
+export function print_result(a: number, b: number): void;
+export function render(a: number, b: number): number;
+export function start(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7befb87695510639(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc7d85022a48ce516(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h6bb5c6d1f2fd64a1(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_start(): void;
